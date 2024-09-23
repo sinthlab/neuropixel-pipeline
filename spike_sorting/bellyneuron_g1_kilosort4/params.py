e509223c583f98aa910a9c8fa87038c7c0cae9ab
Labtech <reza.asri72@yahoo.com>
2024-09-23 16:32:02 -0700

    integration with spikeinterface

---
+++ spike_sorting/bellyneuron_g1_kilosort4/params.py
@@ -0,0 +1,6 @@
+dat_path = 'D:/SGL_DATA/test-day1- spine-bellyneuron_g1/test-day1- spine-bellyneuron_g1_imec0/test-day1- spine-bellyneuron_g1_t0.imec0.ap.bin'
+n_channels_dat = 385
+dtype = 'int16'
+offset = 0
+sample_rate = 30000
+hp_filtered = False
